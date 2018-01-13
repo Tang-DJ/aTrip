1170d2035ea553a7aa8539d0d3f359552e10a043
tangdaijia <253141924@qq.com>
2018-01-14 03:51:45 +0800

    改变样式

--- out/artifacts/web_war_exploded/js/index.js
+++ out/artifacts/web_war_exploded/js/index.js
@@ -6,14 +6,6 @@ $(document).ready(function(){
             columnWidth: '.item',
             itemSelector: '.item'
         });
-    });
+ }
+
 });
-reverseWords('The quick brown fox jumps over the lazy dog.');
-function reverseWords(str) {
-    var re = "";
-    var temp = str.split(" ");
-    for(var i=0;i<temp.length;i++){
-        re = re + temp[i].split("").reverse().join("");
-    }
-    return re;
-}
